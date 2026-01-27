18ebb54c2e3ef0e45519c5c910f1491c0921addb
alaquoke <alaquoke@gmail.com>
2026-01-27 17:15:41 -0300

    fix: removendo proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     vue(),
     vuetify({
